e4f9216adae672fd130762c1fcf17ca03e340660
LaTaevia Berry <lataevia.berry@gmail.com>
2017-09-05 08:42:39 -0500

    add list component and update root module

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,14 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from 'list/list.component';
 
 @NgModule({
+  declarations: [
+    AppComponent
+    ListComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
