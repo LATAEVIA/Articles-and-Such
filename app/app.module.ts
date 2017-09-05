5f13b6c9999456ed3f5550c7a82abdc5921ef2c8
LaTaevia Berry <lataevia.berry@gmail.com>
2017-09-04 23:56:56 -0500

    Commit root component and module skeleton

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { DatepickerModule, AlertModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [BrowserModule],
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AlertModule.forRoot(),
-    DatepickerModule.forRoot()
-  ],
   providers: [],
   bootstrap: [AppComponent]
 })
