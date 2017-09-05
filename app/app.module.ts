2f82df4b88fbd8cbb3064c57b2d0a7ae5b2d40fe
LaTaevia Berry <lataevia.berry@gmail.com>
2017-09-05 08:43:19 -0500

    update directory syntax

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ListComponent } from 'list/list.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
